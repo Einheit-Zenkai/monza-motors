6dbe80d6d9f8717b1a762702553ed031ed9fe9f9
AditiSinghal11 <aditi247ps@gmail.com>
2025-05-06 00:39:47 +0530

    fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -120,17 +120,3 @@ function App() {
 }
 
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Checkout from "./pages/Checkout";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/checkout" element={<Checkout />} />
-        {/* other routes */}
-      </Routes>
-    </Router>
-  );
-}
-export default App;
